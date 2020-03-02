0dc57dabd7d0de71780a0a365519c784bffa1b3e
elikantor <eli.kantor@outlook.com>
2020-03-02 10:16:53 -0500

    pushing minor seed and user controller changes before ActionMailer retrofit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,15 +13,15 @@ class UsersController < ApplicationController
             render json: {user: UserSerializer.new(@user), token: wristband}
         
             # respond_to do |format|
-                if @user.valid?
-                  # Tell the UserMailer to send a welcome email after save
-                  UserMailer.with(user: @user).welcome_email.deliver_now
-                  format.html { redirect_to(@user, notice: 'User was successfully created.') }
-                  format.json { render json: @user, status: :created, location: @user }
-                else
-                  format.html { render action: 'new' }
-                  format.json { render json: @user.errors, status: :unprocessable_entity }
-                end
+                # if @user.valid?
+                #   # Tell the UserMailer to send a welcome email after save
+                #   UserMailer.with(user: @user).welcome_email.deliver_now
+                #   format.html { redirect_to(@user, notice: 'User was successfully created.') }
+                #   format.json { render json: @user, status: :created, location: @user }
+                # else
+                #   format.html { render action: 'new' }
+                #   format.json { render json: @user.errors, status: :unprocessable_entity }
+                # end
             # end
         
         else
