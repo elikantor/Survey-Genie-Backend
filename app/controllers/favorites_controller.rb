d0bf6a2056ffbd832c1ab0a0d9077140792df668
elikantor <eli.kantor@outlook.com>
2020-03-10 10:58:18 -0400

    fixed favorite delete routing

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -10,10 +10,15 @@ class FavoritesController < ApplicationController
         render json: @favorite
     end
 
+    def show
+        favorite = Favorite.find_by(id: params[:id])
+        render json: favorite
+    end
+
     def destroy
         # byebug
-        @favorite = Favorite.find_by(id: params[:id])
-        @favorite.destroy
+        favorite = Favorite.find_by(id: params[:id])
+        favorite.destroy
         render json: {success: "Destroyed"}
     end
 
